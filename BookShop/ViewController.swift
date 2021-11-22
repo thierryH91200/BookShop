edb825a4d9a002d7d577540c0f062cbba7fd1ecc
thierry <thierry.hentic@gmail.com>
2021-11-22 10:13:45 +0100

    Initial Commit

---
+++ BookShop/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  BookShop
+//
+//  Created by thierryH24 on 22/11/2021.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
